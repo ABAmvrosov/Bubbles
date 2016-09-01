86946fb6a4b39636ef99636269183974801691b6
abamvrosov <abamvrosov@gmail.com>
2016-09-01 13:41:59 +0300

    +MainStateMachine +Messenger

---
+++ Assets/Scripts/MainStateMachine/IMainState.cs
@@ -0,0 +1,5 @@
+﻿using System.Collections;
+
+public interface IMainState  {
+    IEnumerator GetCoroutine();
+}
---
+++ Assets/Scripts/MainStateMachine/MainStateMachine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MainStateMachine : MonoBehaviour {
+    
+    private IMainState CurrentState
+    {
+        get { return _currentState; }
+        set {
+            _currentState = value;
+            _currentCoroutine = _currentState.GetCoroutine();
+        }
+    }
+    private IMainState _currentState;
+    private IEnumerator _currentCoroutine;
+
+    private void Awake() {
+        if (CurrentState == null) {
+            CurrentState = new MainMenuLoadState();
+        }
+        StartCoroutine(_currentCoroutine);
+    }
+        
+    public void ChangeState(IMainState state) {
+        StopCoroutine(_currentCoroutine);
+        CurrentState = state;
+        StartCoroutine(_currentCoroutine);
+    }    
+}
